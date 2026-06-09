b237a11c7627a1fb4663ce6ca0f9f300f3716f87
mohammad2001sayed-web <mohammad2001sayed@gmail.com>
2026-06-09 16:55:50 +0300

    fix responnsive layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,45 +1,33 @@
-//! 1) show and hide sections
+//!(1) Show & Hide Sections
 const sections = document.querySelectorAll("section");
-const launchesLink = document.querySelector('a[data-section="launches"]');
-const planetsLink = document.querySelector('a[data-section="planets"]');
-const todayLink = document.querySelector('a[data-section="today-in-space"]');
+const links = document.querySelectorAll(".nav-link");
+
 function showSection(id) {
+  // اخفي كل السكاشن
   sections.forEach((section) => {
     section.classList.add("hidden");
   });
 
+  // اظهر السكشن المطلوب
   document.getElementById(id).classList.remove("hidden");
 }
 
-launchesLink.addEventListener("click", (e) => {
-  e.preventDefault();
-  showSection("launches");
-});
-
-planetsLink.addEventListener("click", (e) => {
-  e.preventDefault();
-  showSection("planets");
-});
-
-todayLink.addEventListener("click", (e) => {
-  e.preventDefault();
-  showSection("today-in-space");
-});
-//* active links
-const links = document.querySelectorAll("[data-section]");
-
+//& Active Link + Navigation
 links.forEach((link) => {
   link.addEventListener("click", (e) => {
     e.preventDefault();
 
-    // شيل active من الكل
+    // شيل الـ active من كل اللينكات
     links.forEach((item) => {
       item.classList.remove("bg-blue-500/10", "text-blue-400");
+      item.classList.add("text-slate-300");
     });
 
-    // ضيف active للحالي
+    // حط active على اللينك الحالي
+    link.classList.remove("text-slate-300");
     link.classList.add("bg-blue-500/10", "text-blue-400");
 
-    showSection(link.dataset.section);
+    // اعرض السكشن المطلوب
+    showSection(link .getAttribute("data-section"));
   });
 });
