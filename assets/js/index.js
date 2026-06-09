72b28f0045c94bb07f0cdaf75afc9e40df6a0bc9
mohammad2001sayed-web <mohammad2001sayed@gmail.com>
2026-06-09 16:40:38 +0300

    fix responnsive layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,33 @@
-// WRITE YOUR JS CODE HERE
+//!(1) Show & Hide Sections
+const sections = document.querySelectorAll("section");
+const links = document.querySelectorAll(".nav-link");
+
+function showSection(id) {
+  // اخفي كل السكاشن
+  sections.forEach((section) => {
+    section.classList.add("hidden");
+  });
+
+  // اظهر السكشن المطلوب
+  document.getElementById(id).classList.remove("hidden");
+}
+
+//& Active Link + Navigation
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    // شيل الـ active من كل اللينكات
+    links.forEach((item) => {
+      item.classList.remove("bg-blue-500/10", "text-blue-400");
+      item.classList.add("text-slate-300");
+    });
+
+    // حط active على اللينك الحالي
+    link.classList.remove("text-slate-300");
+    link.classList.add("bg-blue-500/10", "text-blue-400");
+
+    // اعرض السكشن المطلوب
+    showSection(link.dataset.section);
+  });
+});
